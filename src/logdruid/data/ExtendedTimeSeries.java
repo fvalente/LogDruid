98e8b48a1e25e824190f6bf26ffbb23a2b493ae9
Fred <frederic.valente@gmail.com>
2015-02-10 14:37:20 +0100

    Added missing source code

---
+++ src/logdruid/data/ExtendedTimeSeries.java
@@ -0,0 +1,42 @@
+package logdruid.data;
+
+import org.jfree.data.time.FixedMillisecond;
+import org.jfree.data.time.TimeSeries;
+
+public class ExtendedTimeSeries {
+
+	TimeSeries timeSeries;
+	int[] stat;
+
+	public ExtendedTimeSeries() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExtendedTimeSeries(TimeSeries ts, int[] stats) {
+		stat = stats;
+		timeSeries = ts;
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExtendedTimeSeries(String name, Class<FixedMillisecond> class1) {
+		timeSeries = new TimeSeries(name, class1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TimeSeries getTimeSeries() {
+		return timeSeries;
+	}
+
+	public void setTimeSeries(TimeSeries timeSeries) {
+		this.timeSeries = timeSeries;
+	}
+
+	public int[] getStat() {
+		return stat;
+	}
+
+	public void setStat(int[] stat) {
+		this.stat = stat;
+	}
+
+}
