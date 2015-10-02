afaf9f1185a13335879e639e8373076ef07e069d
Fred <frederic.valente@gmail.com>
2015-10-02 13:40:21 +0200

    Fixed issue during creation of new ReportRecording

--- src/logdruid/data/record/ReportRecording.java
+++ src/logdruid/data/record/ReportRecording.java
@@ -19,9 +19,10 @@ public class ReportRecording extends Recording {
 	private static Logger logger = Logger.getLogger(ReportRecording.class.getName());
 	private String dateFormat;
 	private String subType;
-	private int top100Index=-1;
+	private int top100Index = -1;
 
-	public ReportRecording(String _name, String _regexp, String _exampleLine, String _dateFormat, Boolean _isActive, ArrayList<RecordingItem> _recordingItem,String subType1, boolean _caseSensitive) {
+	public ReportRecording(String _name, String _regexp, String _exampleLine, String _dateFormat, Boolean _isActive, ArrayList<RecordingItem> _recordingItem,
+			String subType1, boolean _caseSensitive) {
 		setName(_name);
 		setRegexp(_regexp);
 		setExampleLine(_exampleLine);
@@ -29,7 +30,7 @@ public class ReportRecording extends Recording {
 		setCaseSensitive(_caseSensitive);
 		dateFormat = _dateFormat;
 		recordingItem = _recordingItem;
-		subType=subType1;
+		subType = subType1;
 		super.id = generate();
 		computeTop100RecordID();
 		logger.info("New ReportRecording name: " + _name + ", regexp: " + _regexp + ", id: " + super.id);
@@ -42,33 +43,38 @@ public class ReportRecording extends Recording {
 	}
 
 	public void setSubType(String subType1) {
-		this.subType=subType1;
+		this.subType = subType1;
 	}
+
 	public String getSubType() {
 		return subType;
 	}
-	
-	private void computeTop100RecordID(){
-	Iterator<RecordingItem> rIIte=recordingItem.iterator();
-	top100Index=-1;
-	int count=0;
-	while (rIIte.hasNext() && top100Index==-1){
-		if (rIIte.next().getProcessingType().equals("top100")){
-			top100Index=count;
+
+	private void computeTop100RecordID() {
+		if (recordingItem != null) {
+			Iterator<RecordingItem> rIIte = recordingItem.iterator();
+			top100Index = -1;
+			int count = 0;
+			while (rIIte.hasNext() && top100Index == -1) {
+				if (rIIte.next().getProcessingType().equals("top100")) {
+					top100Index = count;
+				}
+				count++;
+			}
 		}
-		count++;
-	}}
-	
-	public int getTop100RecordID(){
-		if (top100Index==-1){
-			computeTop100RecordID();;
+	}
+
+	public int getTop100RecordID() {
+		if (top100Index == -1) {
+			computeTop100RecordID();
+			;
 			return top100Index;
 		} else {
 			return top100Index;
 		}
-		
+
 	}
-	
+
 	public String getDateFormat() {
 		return dateFormat;
 	}
@@ -81,20 +87,21 @@ public class ReportRecording extends Recording {
 		this.recordingItem = recordingItem;
 	}
 
-	public void update(String txtName, String txtRegularExp, String exampleLine, String _dateFormat, Boolean _isActive, ArrayList<RecordingItem> _recordingItem, String subType1, boolean _caseSensitive) {
+	public void update(String txtName, String txtRegularExp, String exampleLine, String _dateFormat, Boolean _isActive,
+			ArrayList<RecordingItem> _recordingItem, String subType1, boolean _caseSensitive) {
 		setName(txtName);
 		setRegexp(txtRegularExp);
 		setExampleLine(exampleLine);
 		setIsActive(_isActive);
 		setCaseSensitive(_caseSensitive);
 		dateFormat = _dateFormat;
-		subType=subType1;
+		subType = subType1;
 		recordingItem = _recordingItem;
 		if (this.id == null) {
 			super.id = generate();
 		}
 		computeTop100RecordID();
-		
+
 	}
 
 	public Recording duplicate() {
@@ -105,15 +112,15 @@ public class ReportRecording extends Recording {
 		String _regexp = getRegexp().toString();
 		String _exampleLine = getExampleLine().toString();
 		String _dateFormat = dateFormat.toString();
-		String _subType=subType.toString();
+		String _subType = subType.toString();
 
 		Boolean _isActive = getIsActive().booleanValue();
 		Boolean _caseSensitive = isCaseSensitive();
-		
+
 		if (recordingItem != null) {
 			_recordingItem = (ArrayList<RecordingItem>) recordingItem.clone();
 		}
-		ReportRecording eR=new ReportRecording(_name, _regexp, _exampleLine, _dateFormat, _isActive, _recordingItem, _subType, _caseSensitive);
+		ReportRecording eR = new ReportRecording(_name, _regexp, _exampleLine, _dateFormat, _isActive, _recordingItem, _subType, _caseSensitive);
 		eR.setDateFormatID(this.getDateFormatID());
 		return eR;
 	}
